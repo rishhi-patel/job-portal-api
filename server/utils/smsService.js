d0f30075268522008cfda07b2bea14af396edde3
harsh viradiya <harshviradiya2000@gmail.com>
2023-07-25 10:34:47 +0530

    -update sms code

--- server/utils/smsService.js
+++ server/utils/smsService.js
@@ -3,13 +3,18 @@ const client = require("twilio")(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
 
 const smsService = {
   sendOtpToMobile: async (mobileNo, otp) => {
-    const args = {
-      body: `Your OTP for login to Jenny Point is ${otp} Please do not share this OTP.Thank you.`,
-      from: "+14067408483",
-      to: `+91${mobileNo}`,
-    };
-    const res = await client.messages.create(args);
-    return res;
+    try {
+      var args = {
+        from: "+14067408483",
+        to: `+91 ${mobileNo}`,
+        body: `Your OTP for login to Jenny Point is ${otp} Please do not share this OTP.Thank you.`,
+      };
+
+      const res = await client.messages.create(args);
+      return res;
+    } catch (error) {
+      throw new Error(error);
+    }
   },
 
   generateOTP: () => Math.floor(100000 + Math.random() * 900000) || 987654,
