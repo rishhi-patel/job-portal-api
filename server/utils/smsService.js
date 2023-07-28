619af4cad874ff8ac46fbdae0f254c0d7cf1f041
harsh viradiya <harshviradiya2000@gmail.com>
2023-07-28 20:14:26 +0530

    Revert "NEW : SMS Service"
    This reverts commit b571d5e139645d03e5dfa88054703aee98a52381.

--- server/utils/smsService.js
+++ server/utils/smsService.js
@@ -3,18 +3,13 @@ const client = require("twilio")(process.env.ACCOUNTSID, process.env.AUTHTOKEN)
 
 const smsService = {
   sendOtpToMobile: async (mobileNo, otp) => {
-    try {
-      var args = {
-        from: "+14067408483",
-        to: `+91 ${mobileNo}`,
-        body: `Your OTP for login to Jenny Point is ${otp} Please do not share this OTP.Thank you.`,
-      }
-
-      const res = await client.messages.create(args)
-      return res
-    } catch (error) {
-      throw new Error(error)
+    const args = {
+      body: `Your OTP for login to Jenny Point is ${otp} Please do not share this OTP.Thank you.`,
+      from: "+14067408483",
+      to: `+91${mobileNo}`,
     }
+    const res = await client.messages.create(args)
+    return res
   },
 
   generateOTP: () => Math.floor(100000 + Math.random() * 900000) || 987654,
