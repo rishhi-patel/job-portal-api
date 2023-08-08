01ddcaf9d0165db2a8d386a4f86ded5bedb21b28
harsh viradiya <harshviradiya2000@gmail.com>
2023-08-08 18:33:18 +0530

    --update sms service

--- server/utils/smsService.js
+++ server/utils/smsService.js
@@ -1,24 +1,24 @@
-const twilio = require("twilio")
-require("dotenv").config()
+const twilio = require("twilio");
+require("dotenv").config();
 
 const smsService = {
   sendOtpToMobile: async (mobileNo, otp) => {
     try {
-      const client = twilio(process.env.ACCOUNTSID, process.env.AUTHTOKEN)
+      const client = twilio(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
       var args = {
         from: "+14067408483",
-        to: `+91 ${mobileNo}`,
+        to: `${mobileNo}`,
         body: `Your OTP for login to Job Portal App is ${otp} Please do not share this OTP.Thank you.`,
-      }
+      };
 
-      const res = await client.messages.create(args)
-      return res
+      const res = await client.messages.create(args);
+      return res;
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
   },
 
   generateOTP: () => Math.floor(100000 + Math.random() * 900000) || 987654,
-}
+};
 
-module.exports = smsService
+module.exports = smsService;
